02af14475dca68b666874388c035b34d55283ee7
Micah Hawkins <micahbhawkins@gmail.com>
2017-02-01 16:25:35 -0800

    basic TEA

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,7 +1,36 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (Html, text, div)
 
 
 main =
-    text "Hello Elm!"
+    Html.program
+        { init = createModel
+        , update = update
+        , view = view
+        , subscriptions = (\_ -> Sub.none)
+        }
+
+
+type alias Model =
+    {}
+
+
+createModel : ( Model, Cmd Msg )
+createModel =
+    ( {}, Cmd.none )
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text "Hello Micah!" ]
