0333e05a00ed83a0ba127118f2e96d25415d0c46
Micah Hawkins <micahbhawkins@gmail.com>
2017-02-02 21:58:32 -0800

    adds some flippin flippin

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,10 +1,12 @@
 module Main exposing (..)
 
 import Html exposing (Html, text, div)
+import Html.Events exposing (onClick)
 import Html.Attributes exposing (class, classList)
 import Random
 
 
+main : Program Never Model Msg
 main =
     Html.program
         { init = createModel
@@ -20,6 +22,8 @@ main =
 
 type Model
     = Playing Deck
+    | Guessing Deck Card
+    | MatchCard Deck Card Card
 
 
 type alias Deck =
@@ -55,7 +59,7 @@ initCard : Group -> String -> Card
 initCard group name =
     { id = name
     , group = group
-    , flipped = True
+    , flipped = False
     }
 
 
@@ -90,6 +94,7 @@ createModel =
 type Msg
     = NoOp
     | Shuffle (List Int)
+    | Flip Card
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -105,6 +110,20 @@ update msg model =
             in
                 ( Playing newDeck, Cmd.none )
 
+        Flip card ->
+            if card.flipped then
+                model ! []
+            else
+                checkIfCorrect card model
+
+
+flip : Bool -> Card -> Card -> Card
+flip isFlipped a b =
+    if (a.id == b.id) && (a.group == b.group) then
+        { b | flipped = isFlipped }
+    else
+        b
+
 
 shuffleDeck : Deck -> List comparable -> Deck
 shuffleDeck deck xs =
@@ -112,6 +131,40 @@ shuffleDeck deck xs =
         |> List.sortBy Tuple.second
         |> List.unzip
         |> Tuple.first
+
+
+checkIfCorrect : Card -> Model -> ( Model, Cmd Msg )
+checkIfCorrect card model =
+    case model of
+        Playing deck ->
+            let
+                newDeck =
+                    List.map (flip True card) deck
+            in
+                Guessing newDeck card ! []
+
+        Guessing deck guess ->
+            let
+                newDeck =
+                    List.map (flip True card) deck
+
+                newModel =
+                    MatchCard newDeck guess card
+            in
+                newModel ! []
+
+        MatchCard deck guess1 guess2 ->
+            if guess1.id == guess2.id then
+                update (Flip card) (Playing deck)
+            else
+                let
+                    flipGuess =
+                        flip False guess1 >> flip False guess2
+
+                    newDeck =
+                        List.map flipGuess deck
+                in
+                    Playing newDeck ! []
 
 
 
@@ -122,13 +175,30 @@ view : Model -> Html Msg
 view model =
     case model of
         Playing deck ->
-            div [ class "wrapper" ] (List.map createCard deck)
+            game deck
+
+        Guessing deck _ ->
+            game deck
+
+        MatchCard deck _ _ ->
+            game deck
+
+
+game : Deck -> Html Msg
+game deck =
+    div [ class "wrapper" ] (List.map createCard deck)
 
 
 createCard : Card -> Html Msg
 createCard card =
     div [ class "container" ]
-        [ div [ classList [ ( "card", True ), ( "flipped", card.flipped ) ] ]
+        [ div
+            [ classList
+                [ ( "card", True )
+                , ( "flipped", card.flipped )
+                ]
+            , onClick (Flip card)
+            ]
             [ div [ class "card-back" ] []
             , div [ class ("front " ++ cardClass card) ] []
             ]
